da342c723de519783bb2d6ca670d56ed2d98e729
Kathy Hoang <kathyhahoang@gmail.com>
2023-07-14 12:37:37 -0500

    Creates AppLayout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Company</Link>
+      <p>Kathy</p>
+    </header>
+  );
+}
+
+export default Header;
