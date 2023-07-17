eb96a7fc907f2c96e7b3ea128ee18ed101f6c7d5
Kathy Hoang <kathyhahoang@gmail.com>
2023-07-17 15:37:36 -0500

    Creates grid layout on AppLayout

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Kathy</div>;
+  return <div className="hidden text-sm font-semibold md:block">Kathy</div>;
 }
 
 export default Username;
